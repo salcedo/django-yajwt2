1811ede0d5a801f328ce5801ac429f9ff957efa2
Brian Salcedo <brian@salcedo.tech>
2020-08-01 10:42:42 -0400

    refactor for seamless integration with DRF

--- django_yajwt/middleware.py
+++ django_yajwt/middleware.py
@@ -1,13 +1,12 @@
-import logging
-
-from django.contrib.auth.models import AnonymousUser
-
 from django_yajwt.auth import JWTAuthentication
 
 from jwt import PyJWTError
 
 
-logger = logging.getLogger(__name__)
+try:
+    from rest_framework.exceptions import AuthenticationFailed
+except ImportError:
+    AuthenticationFailed = None
 
 
 class JWTAuthenticationMiddleware:
@@ -19,22 +18,41 @@ class JWTAuthenticationMiddleware:
         if request.user.is_authenticated:
             return self.get_response(request)
 
-        user = None
-        try:
-            authorization = request.headers.get('Authorization', None)
-            if not authorization:
-                raise KeyError
-            if not authorization.startswith(self.jwt_auth.token_prefix):
-                raise ValueError
-
-            token = authorization.split(self.jwt_auth.token_prefix)[1]
-            payload = self.jwt_auth.decode_jwt(token)
-            user = self.jwt_auth.get_user(payload['sub'])
-            print('user is good')
-        except (IndexError, KeyError, ValueError, PyJWTError) as e:
-            logger.debug('token missing or invalid')
-
+        user = self._validate_authorization_token(request)
         if user is not None:
             request.user = user
 
         return self.get_response(request)
+
+    def authenticate(self, request):
+        if request.user.is_authenticated:
+            return (request.user, None)
+
+        user = self._validate_authorization_token(request)
+        if user is None:
+            if AuthenticationFailed is not None:
+                raise AuthenticationFailed(_('Authentication failed'))
+            else:
+                raise
+
+        return (user, None)
+
+    def authenticate_header(self, request):
+        return self.jwt_auth.token_prefix
+
+    def _validate_authorization_token(self, request):
+        user = None
+        try:
+            authorization = request.headers.get('Authorization', None)
+            if authorization is None:
+                return None
+            if not authorization.startswith(self.jwt_auth.token_prefix):
+                return None
+
+            token = authorization.split(self.jwt_auth.token_prefix)[1]
+            payload = self.jwt_auth.decode_jwt(token)
+            user = self.jwt_auth.get_user(payload['sub'])
+        except (IndexError, KeyError, PyJWTError):
+            return None
+
+        return user
